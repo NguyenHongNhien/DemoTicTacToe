e39dc2ffa3c99b929065eccb5d800be9ba81dc99
pikkupr <prateekkrjain@hotmail.com>
2020-05-03 21:20:49 +0530

    Initial commit

---
+++ Assets/Scripts/GameScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScript : MonoBehaviour
+{
+    public string gameMode;
+
+    public void Awake()
+    {
+        GameObject persistantObj = GameObject.FindGameObjectWithTag("GameMode") as GameObject;
+        gameMode = persistantObj.GetComponent<PersistanceScript>().gameMode;
+        print(gameMode);
+        Destroy(persistantObj);
+    }
+}
---
+++ Assets/Scripts/PlayButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayButton : MonoBehaviour
+{
+    public string gameMode;
+    public GameObject PersistantObject;
+
+    public void OnMouseDown()
+    {
+        PersistantObject.GetComponent<PersistanceScript>().gameMode = gameMode;
+        SceneManager.LoadScene("GameScreen");
+    }
+}
